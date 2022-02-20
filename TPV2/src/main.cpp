37d4d575e7f37e9bae140bca94483d241ce4867a
Javier V <javilleg@ucm.es>
2022-02-20 13:23:32 +0100

    Pantalla completa hecho bien

--- TPV2/src/main.cpp
+++ TPV2/src/main.cpp
@@ -18,31 +18,25 @@ int main(int ac, char **av) {
 
 	auto& sdl = *SDLUtils::instance();
 
-	/*
-	int wWidth;
-	int wHeight;
-
-	SDL_GetRendererOutputSize(sdl.renderer(), &wWidth, &wHeight);
-
-	std::cout << wWidth << std::endl;
-	*/
-
+	//Obtenemos el tamaño de la pantalla
 	SDL_DisplayMode DM;
-
 	SDL_GetDesktopDisplayMode(0, &DM);
 
-	//std::cout << DM.w << std::endl;
-
-	sdl.toggleFullScreen();
-
+	/*
 	float scaleX = (float)DM.w / sdl.width();
 	float scaleY = (float)DM.h / sdl.height();
 
 	SDL_RenderSetScale(sdl.renderer(), scaleX, scaleY);
+	*/
 
+	//Escalamos toda la ventana para que se ajuste al tamaño de la pantalla
 	SDL_RenderSetLogicalSize(sdl.renderer(), DM.w, DM.h);
 
-	std::cout << sdl.width() << std::endl;
+	//Cambiamos el tamaño de la ventana
+	SDL_SetWindowSize(sdl.window(), DM.w, DM.h);
+
+	//Ponemos en pantalla completa
+	sdl.toggleFullScreen();
 
 	//show the cursor
 	sdl.showCursor();
@@ -58,10 +52,10 @@ int main(int ac, char **av) {
 
 	b2World world = b2World(gravity);
 
+	//Definimos un objeto (dinámico)
 	float width = 5.f;
 	float height = 5.f;
 
-	//Definimos un objeto (dinámico)
 	b2BodyDef groundBodyDef;
 	groundBodyDef.position.Set(80.0f, 45.0f);
 	groundBodyDef.type = b2_dynamicBody;
